6d507bf1b860c05ce002cab6b04ddc0204062a1d
Avison <avisonlivingstone@gmail.com>
2019-05-18 08:28:17 +0100

    Day 8 Task and Solution

---
+++ day8.py
@@ -0,0 +1,24 @@
+# Task 
+# Given an array, A, of N integers, print A's elements in reverse order as a single line of space-separated numbers.
+
+# Input Format
+
+# The first line contains an integer, N (the size of our array). 
+# The second line contains N space-separated integers describing array A's elements.
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+     input()
+
+arry = str(input()).split(" ")
+arry.reverse()
+
+for i in arr:
+    print(i + " ", end="")
