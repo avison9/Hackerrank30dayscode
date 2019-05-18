22c5b7cddd29ebdff31e713173aff8bc466d1af4
Avison <avisonlivingstone@gmail.com>
2019-05-18 08:08:55 +0100

    Day 6 Task Question updated

--- day6.py
+++ day6.py
@@ -1,3 +1,6 @@
+# Task 
+# Given an integer, n, print its first 10 multiples. Each multiple n x i  (where 1 <= i >=10) should be printed on a new line in the form: n x i = result.
+
 n = int(input())
 while n >= 2 and n <= 20:
 	for i in range (1,11):
